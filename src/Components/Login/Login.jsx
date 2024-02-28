9960a90a52cd420a4de3e3aa591f2dfc940b861c
Vittor Rafael <rafaelsales202205@gmail.com>
2024-02-28 11:36:14 -0300

    first commit

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginPasswordReset from './LoginPasswordReset';
+import { UserContext } from '../../UserContext';
+
+const Login = () => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Navigate to="/conta" />;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Login;
